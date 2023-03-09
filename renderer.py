8395a6d6c0c1d610cf61db4251ef9f809ff8ecfb
Dmitri <codedintellect@gmail.com>
2023-03-08 22:04:23 -0600

    Moved frame render to different script Now only rendering new frames when needed (could cause problems in the future?)

---
+++ renderer.py
@@ -0,0 +1,72 @@
+import pygame
+import resources
+from config import TILE_SIZE
+
+BOARD_SIZE = TILE_SIZE * 8
+game = None
+selected = None
+
+# COLORS (taken with shame directly from lichess :D)
+selected_color = (20, 85, 30, 127)
+last_move_color = (155, 199, 0, 105)
+
+def flip(x, y, game):
+  if game.plr_color: # TRUE == WHITE
+    return x, 7 - y
+  else:
+    return 7 - x, y
+
+def render_pieces(window, game, selected):
+  for sq in range(64):
+    p = game.piece(sq)
+    if p:
+      x, y = flip(sq % 8, sq // 8, game)
+      icon = resources.pieces[p.symbol()]
+      if sq == selected and pygame.mouse.get_pressed()[0]:
+        icon = icon.copy()
+        icon.set_alpha(128)
+      window.blit(icon, (x * TILE_SIZE, y * TILE_SIZE))
+  if selected is not None and pygame.mouse.get_pressed()[0]:
+    icon = resources.pieces[game.piece(selected).symbol()]
+    x, y = pygame.mouse.get_pos()
+    window.blit(icon, (x - TILE_SIZE // 2, y - TILE_SIZE // 3))
+
+def highlight_square(surface, sq, color, game):
+  x, y = flip(sq % 8, sq // 8, game)
+  rect = (x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE)
+  pygame.draw.rect(surface, color, rect)
+
+def highlight_circle(surface, sq, color, game):
+  x, y = flip(sq % 8, sq // 8, game)
+  center = ((x + 0.5) * TILE_SIZE, (y + 0.5) * TILE_SIZE)
+  pygame.draw.circle(surface, color, center, TILE_SIZE // 7) # Why 7? No idea...
+
+def render(window, game, selected):
+  # RENDER BACKBOARD
+  window.blit(resources.board, (0,0))
+
+  highlights = pygame.Surface(resources.board.get_size(), pygame.SRCALPHA)
+
+  # RENDER SELECTED
+  if selected:
+    highlight_square(highlights, selected, selected_color, game)
+    for mv in game.get_moves(selected):
+      highlight_circle(highlights, mv.to_square, selected_color, game)
+
+  # RENDER LAST MOVE
+  lm = game.last_move()
+  if lm:
+    highlight_square(highlights, lm.from_square, last_move_color, game)
+    highlight_square(highlights, lm.to_square, last_move_color, game)
+
+  window.blit(highlights, (0,0))
+
+  # RENDER CELL LABELS
+  if True: # TODO: ADD CONFIG
+    if game.plr_color:
+      window.blit(resources.for_white, (0,0))
+    else:
+      window.blit(resources.for_black, (0,0))
+
+  # RENDER PIECES
+  render_pieces(window, game, selected)
--- ui.py
+++ ui.py
@@ -1,41 +1,17 @@
 import pygame
 import threading
 import resources
+from renderer import render, flip
 from config import TILE_SIZE
 
 BOARD_SIZE = TILE_SIZE * 8
 game = None
 selected = None
 
-# COLORS (taken with shame directly from lichess :D)
-selected_color = (20, 85, 30, 127)
-last_move_color = (155, 199, 0, 105)
-
-def player_flip(x, y):
-  if game.plr_color: # TRUE == WHITE
-    return x, 7 - y
-  else:
-    return 7 - x, y
-
-def render_pieces(window):
-  for sq in range(64):
-    p = game.piece(sq)
-    if p:
-      x, y = player_flip(sq % 8, sq // 8)
-      icon = resources.pieces[p.symbol()]
-      if sq == selected and pygame.mouse.get_pressed()[0]:
-        icon = icon.copy()
-        icon.set_alpha(128)
-      window.blit(icon, (x * TILE_SIZE, y * TILE_SIZE))
-  if selected is not None and pygame.mouse.get_pressed()[0]:
-    icon = resources.pieces[game.piece(selected).symbol()]
-    x, y = pygame.mouse.get_pos()
-    window.blit(icon, (x - TILE_SIZE // 2, y - TILE_SIZE // 3))
-
 def mouse(e):
   global selected
   pos = pygame.mouse.get_pos()
-  x, y = player_flip(pos[0] // TILE_SIZE, pos[1] // TILE_SIZE)
+  x, y = flip(pos[0] // TILE_SIZE, pos[1] // TILE_SIZE, game)
   sq = y * 8 + x
   p = game.piece(sq)
   if p == None or not p.color == game.plr_color:
@@ -44,16 +20,6 @@ def mouse(e):
     selected = None
   elif e.type == pygame.MOUSEBUTTONDOWN:
     selected = sq
-
-def highlight_square(surface, sq, color):
-  x, y = player_flip(sq % 8, sq // 8)
-  rect = (x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE)
-  pygame.draw.rect(surface, color, rect)
-
-def highlight_circle(surface, sq, color):
-  x, y = player_flip(sq % 8, sq // 8)
-  center = ((x + 0.5) * TILE_SIZE, (y + 0.5) * TILE_SIZE)
-  pygame.draw.circle(surface, color, center, TILE_SIZE // 7) # Why 7? No idea...
 
 def render_loop():
   clock = pygame.time.Clock()
@@ -67,43 +33,24 @@ def render_loop():
   running = True
   while running:
     clock.tick(60)
+    update_frame = False
     for e in pygame.event.get():
+      # print(pygame.event.event_name(e.type))
       if e.type == pygame.QUIT:
         running = False
       elif e.type == pygame.MOUSEBUTTONDOWN or e.type == pygame.MOUSEBUTTONUP:
+        update_frame = True
         mouse(e)
+      elif e.type == pygame.VIDEOEXPOSE:
+        update_frame = True
+      elif e.type == pygame.MOUSEMOTION:
+        if selected is not None and pygame.mouse.get_pressed()[0]:
+          update_frame = True
 
-    # RENDER BACKBOARD
-    window.blit(resources.board, (0,0))
-
-    highlights = pygame.Surface(resources.board.get_size(), pygame.SRCALPHA)
-
-    # RENDER SELECTED
-    if selected:
-      highlight_square(highlights, selected, selected_color)
-      for mv in game.get_moves(selected):
-        highlight_circle(highlights, mv.to_square, selected_color)
-
-    # RENDER LAST MOVE
-    lm = game.last_move()
-    if lm:
-      highlight_square(highlights, lm.from_square, last_move_color)
-      highlight_square(highlights, lm.to_square, last_move_color)
-
-    window.blit(highlights, (0,0))
-
-    # RENDER CELL LABELS
-    if True: # TODO: ADD CONFIG
-      if game.plr_color:
-        window.blit(resources.for_white, (0,0))
-      else:
-        window.blit(resources.for_black, (0,0))
-
-    # RENDER PIECES
-    render_pieces(window)
-
-    # RENDER SCREEN
-    pygame.display.flip()
+    if update_frame:
+      render(window, game, selected)
+      # Update window
+      pygame.display.flip()
 
   # EXIT WINDOW
   pygame.quit()
